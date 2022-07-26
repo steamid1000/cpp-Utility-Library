ba52fd3609f29183f8f24655c6ea36e4d472dff1
Shivrudra Patil <steamid1000@gmail.com>
2022-07-26 15:15:57 +0530

    First commit of working functions

---
+++ util.h
@@ -0,0 +1,149 @@
+#pragma once 
+#include "bits/stdc++.h"
+
+namespace util
+{
+    template <typename T>
+    void swap(T arr[], int i, int j) //^ a simple swapping function
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    void bubble_sort(int arr[], int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            short swaped = 0; //& will check the number of swaps, if 0 then break
+            for (int j = 0; j < size - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    swap(arr, j, j+1);
+                    swaped++;
+                }
+            }
+
+            if (swaped == 0)
+            {
+                return;
+            }
+        }
+    }
+
+    template <typename T>
+    void displayArray(T arr[], int size = 0) //^ displays the array on console
+    {
+
+        for (int i = 0; i < size; i++)
+        {
+            std::cout << arr[i] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    //& block of the linked list class and fucntion
+    class linkedList //^ I will add the support for other types using template soon
+    {
+
+    public:
+        int val;
+        linkedList *next;
+
+        linkedList() {}
+
+        linkedList(int dat) // creating the first node
+        {
+            val = dat;
+            next = NULL;
+        }
+
+        void insert(linkedList *&head, int val) // simple insert function for the linked list
+        {
+            linkedList *n = new linkedList(val);
+            if (head == NULL)
+            {
+                head = n;
+                return;
+            }
+
+            linkedList *temp = head;
+
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+
+            temp->next = n;
+        }
+    };
+
+    void displayLinkedList(linkedList *head) // displays the linked list
+    {
+        while (head != NULL)
+        {
+            std::cout << head->val << "->";
+            head = head->next;
+        }
+
+        std::cout << "NULL\n";
+    }
+
+    void reverseLL(linkedList *&head) //^ fucntion reverses the linked list in place
+    {
+        linkedList *prev = NULL;
+        linkedList *temp = NULL;
+        linkedList *&current = head;
+
+        while (current != NULL)
+        {
+            temp = current->next; // stores the address of the next node
+            current->next = prev; // currents-> will be null
+            prev = current;       // prev = current node
+            current = temp;       // current will be the next node
+        }
+
+        head = prev;
+    }
+
+    template <typename T>
+    void displayVector(std::vector<T> vec) //^ Can display vectors of general types
+    {
+        for (auto i : vec)
+        {
+            std::cout << i << " ";
+        }
+        std::cout << "\n";
+    }
+
+    bool isPalin(std::string input) //^ function to check if a given string is palindrome or not
+    {
+        int j = input.length() - 1;
+
+        for (int i = 0; i < input.length() / 2; i++)
+        {
+            if (input[i] != input[j])
+                return false;
+
+            j--;
+        }
+
+        return true;
+    }
+
+    template <typename T>
+    bool SearchInArray(T arr[], int size, T element) //^ Can generally be used for int,string and char Arrays or their smaller sub-Arays
+    {                                                //^ which can be determined by the size you give for searching.
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] == element)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
